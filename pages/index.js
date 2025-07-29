e0587f99d1d3b88c32fcd3ae16ef842e3992035f
SolanaStakeCalc <coinfloor99@pm.me>
2025-07-29 17:06:49 +0100

    Update index.js with new login interface

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import useSWR from "swr";
 const fetcher = url => fetch(url).then(res => res.json());
 
 export default function Home() {
-  const { data: session, status } = useSession();
+//  const { data: session, status } = useSession();
+const { data: session } = useSession() ?? {};
   const { data, error } = useSWR('/api/indicators', fetcher);
 
   if (status === "loading") return <p>Loading session...</p>;
